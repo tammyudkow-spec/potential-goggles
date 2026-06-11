0f0eb870d44533f2434113a332e9e020a571251b
Tammy Udkow <tammyudkow@gmail.com>
2026-06-10 18:23:16 -0700

    update About page text

--- demo-app/app/about/page.tsx
+++ demo-app/app/about/page.tsx
@@ -4,7 +4,7 @@ export default function AboutPage() {
       <h1 className="text-4xl font-bold mb-4">About</h1>
       <p className="text-lg text-gray-600">
         Welcome to the About page. This is a simple Next.js app built with the
-        OpenClinTech Clinician Engineer Bootcamp.
+        OpenClinTech Clinician Engineer Bootcamp. Blah blah blah.
       </p>
     </main>
   );
